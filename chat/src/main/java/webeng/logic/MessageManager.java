b396cbf91293d70e6697cc1462f3e3c885470092
Luca Selinski <lucaselinski@gmail.com>
2021-06-21 13:57:18 +0200

    Added delegations from the managers to the DAOs

--- chat/src/main/java/webeng/logic/MessageManager.java
+++ chat/src/main/java/webeng/logic/MessageManager.java
@@ -1,4 +1,36 @@
 package webeng.logic;
 
+import webeng.data.DaoFactory;
+import webeng.data.MessageDao;
+import webeng.transfer.Message;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public class MessageManager {
+    private MessageDao dao;
+
+    public MessageManager() {
+        dao = DaoFactory.getMessageDao();
+    }
+
+    public Message get(String sender, String receiver, Timestamp time) {
+        return dao.get(sender, receiver, time);
+    }
+
+    public List<Message> getAll() {
+        return dao.getAll();
+    }
+
+    public void add(Message message) {
+        dao.add(message);
+    }
+
+    public void update(Message message) {
+        dao.update(message);
+    }
+
+    public void delete(Message message) {
+        dao.delete(message);
+    }
 }
--- chat/src/main/java/webeng/logic/UserManager.java
+++ chat/src/main/java/webeng/logic/UserManager.java
@@ -1,4 +1,35 @@
 package webeng.logic;
 
+import webeng.data.DaoFactory;
+import webeng.data.UserDao;
+import webeng.transfer.User;
+
+import java.util.List;
+
 public class UserManager {
+    private UserDao dao;
+
+    public UserManager() {
+        dao = DaoFactory.getUserDao();
+    }
+
+    public User get(String name) {
+        return dao.get(name);
+    }
+
+    public List<User> getAll() {
+        return dao.getAll();
+    }
+
+    public void add(User user) {
+        dao.add(user);
+    }
+
+    public void update(User user) {
+        dao.update(user);
+    }
+
+    public void delete(User user) {
+        dao.delete(user);
+    }
 }
