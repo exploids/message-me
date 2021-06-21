ed40e61e27c3c2422d6adc6bb5b12e19bf9e104b
Luca Selinski <lucaselinski@gmail.com>
2021-06-21 12:54:04 +0200

    Described the UserDao and MessageDao interfaces

--- chat/src/main/java/webeng/data/MessageDao.java
+++ chat/src/main/java/webeng/data/MessageDao.java
@@ -1,4 +1,18 @@
 package webeng.data;
 
+import webeng.transfer.Message;
+
+import java.sql.Timestamp;
+import java.util.List;
+
 public interface MessageDao {
+    Message get(String sender, String receiver, Timestamp time);
+
+    List<Message> getAll();
+
+    void add(Message message);
+
+    void update(Message message);
+
+    void delete(Message message);
 }
--- chat/src/main/java/webeng/data/UserDao.java
+++ chat/src/main/java/webeng/data/UserDao.java
@@ -1,4 +1,17 @@
 package webeng.data;
 
+import webeng.transfer.User;
+
+import java.util.List;
+
 public interface UserDao {
+    User get(String name);
+
+    List<User> getAll();
+
+    void add(User user);
+
+    void update(User user);
+
+    void delete(User user);
 }
