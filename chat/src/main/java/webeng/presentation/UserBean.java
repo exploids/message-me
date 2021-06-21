5a147318eda063c476888faf71a7a71fd0876d86
Luca Selinski <lucaselinski@gmail.com>
2021-06-21 13:54:30 +0200

    Added the base implementation for UserBean

--- chat/src/main/java/webeng/presentation/UserBean.java
+++ chat/src/main/java/webeng/presentation/UserBean.java
@@ -1,5 +1,7 @@
 package webeng.presentation;
 
+import webeng.transfer.User;
+
 import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -7,4 +9,31 @@ import java.io.Serializable;
 @Named
 @RequestScoped
 public class UserBean implements Serializable {
+    private User user;
+
+    /**
+     * Gets the user of this user bean.
+     *
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the user of this user bean.
+     *
+     * @param user the new user
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void login() {
+
+    }
+
+    public void logout() {
+
+    }
 }
