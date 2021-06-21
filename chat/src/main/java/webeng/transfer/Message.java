3b0788defcc09a46d663069f83858d4300e97ba8
johannesvieler <Miantejo127715>
2021-06-21 13:16:48 +0200

    Transferobjects added

--- chat/src/main/java/webeng/transfer/Message.java
+++ chat/src/main/java/webeng/transfer/Message.java
@@ -1,4 +1,45 @@
 package webeng.transfer;
 
+import java.sql.Timestamp;
+
 public class Message {
+
+    String sender;
+    String receiver;
+    Timestamp time;
+    String text;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
 }
