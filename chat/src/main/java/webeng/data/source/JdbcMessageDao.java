fa215929f3eb1603bc15b083b8fb02a1187660f6
johannesvieler <Miantejo127715>
2021-06-21 19:20:43 +0200

    JdbcMessageDao.java get, add, update and delete-Method added

--- chat/src/main/java/webeng/data/source/JdbcMessageDao.java
+++ chat/src/main/java/webeng/data/source/JdbcMessageDao.java
@@ -3,13 +3,34 @@ package webeng.data.source;
 import webeng.data.MessageDao;
 import webeng.transfer.Message;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.List;
 
 public class JdbcMessageDao extends JdbcBase implements MessageDao {
     @Override
     public Message get(String sender, String receiver, Timestamp time) {
-        return null;
+        Message message = null;
+        try (PreparedStatement statement = getConnection().prepareStatement("select * from Message where = ? and ? and ?`")) {
+            statement.setString(1, sender);
+            statement.setString(2, receiver);
+            statement.setTimestamp(3, time);
+
+            try (ResultSet results = statement.executeQuery()) {
+                if (results.next()) {
+                    message = new Message();
+                    message.setSender(results.getString(1));
+                    message.setReceiver(results.getString(2));
+                    message.setTime(results.getTimestamp(3));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return message;
     }
 
     @Override
@@ -19,16 +40,37 @@ public class JdbcMessageDao extends JdbcBase implements MessageDao {
 
     @Override
     public void add(Message message) {
-
+        try (PreparedStatement statement = getConnection().prepareStatement("insert into Message values (?, ?, ?)")) {
+            statement.setString(1, message.getSender());
+            statement.setString(2, message.getReceiver());
+            statement.setTimestamp(3, message.getTime());
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void update(Message message) {
-
+        try (PreparedStatement statement = getConnection().prepareStatement("update Message set sender = ?, receiver = ? where sender = ?")) {
+            statement.setString(1, message.getSender());
+            statement.setString(2, message.getReceiver());
+            statement.setTimestamp(3, message.getTime());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(Message message) {
-
+        try (PreparedStatement statement = getConnection().prepareStatement("delete from Message where name = ? and  receiver = ? and time = ?")) {
+            statement.setString(1, message.getSender());
+            statement.setString(2, message.getReceiver());
+            statement.setTimestamp(3, message.getTime());
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
